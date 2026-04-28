489001805b1b4e2e53abe6430dbcd9d9f2c522e1
abhishekdav003 <abhishekdav003@gmail.com>
2026-04-29 01:47:57 +0530

    Task 1 Done

---
+++ Task1/index.js
@@ -0,0 +1,14 @@
+const mainHeading = document.getElementById("main-heading");
+mainHeading.textContent = "Fruit World";
+
+mainHeading.style.color = "orange";
+
+const header = document.getElementById("header");
+header.style.background = "green";
+
+header.style.borderBottom = "5px solid orange";
+
+const thanksDiv = document.getElementById("thanks");
+const para = document.createElement("p");
+para.textContent = "Please visit us again";
+thanksDiv.appendChild(para);
