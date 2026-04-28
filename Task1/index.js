bd7d8a857d6989f1f2a42aa754b9fdfd0a1e703f
abhishekdav003 <abhishekdav003@gmail.com>
2026-04-29 01:54:34 +0530

    issue in task1 solved

--- Task1/index.js
+++ Task1/index.js
@@ -8,6 +8,9 @@ header.style.background = "green";
 
 header.style.borderBottom = "5px solid orange";
 
+const basketHeading = document.getElementById("basket-heading");
+basketHeading.style.color = "green";
+
 const thanksDiv = document.getElementById("thanks");
 const para = document.createElement("p");
 para.textContent = "Please visit us again";
