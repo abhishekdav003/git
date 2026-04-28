f10185d3b73b3e06c2f95b73abc6e849a00ee9d2
abhishekdav003 <abhishekdav003@gmail.com>
2026-04-29 02:08:30 +0530

    Task 3 Done

---
+++ Task3/index.js
@@ -0,0 +1,8 @@
+// Write your code below:
+const items = document.getElementsByTagName("li");
+
+items[4].style.color = "red";
+
+for (let i = 0; i < items.length; i++) {
+  items[i].style.fontStyle = "italic";
+}
