08a8c05cba72c7f8992be0f4cb268ffa9724a0af
abhishekdav003 <abhishekdav003@gmail.com>
2026-04-29 02:04:18 +0530

    Task 2 Done

---
+++ Task2/index.js
@@ -0,0 +1,8 @@
+// Write your code below:
+const fruits = document.getElementsByClassName("fruit");
+
+fruits[2].style.backgroundColor = "yellow";
+
+for (let i = 0; i < fruits.length; i++) {
+  fruits[i].style.fontWeight = "bold";
+}
